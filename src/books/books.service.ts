0247ea096a062a7196d612203ab93f157b5d9557
Akhilesh H <akhilesh@revyrieglobal.com>
2022-06-10 19:43:14 +0530

    Throws error if FKey dependencies aren't met

--- src/books/books.service.ts
+++ src/books/books.service.ts
@@ -1,6 +1,8 @@
 import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
-import { Repository } from 'typeorm';
+import { Author } from 'src/authors/entities/author.entity';
+import { Category } from 'src/categories/entities/category.entity';
+import { EntityNotFoundError, Repository } from 'typeorm';
 import { CreateBookDto } from './dto/create-book.dto';
 import { FilterBookDto } from './dto/filter-book.dto';
 import { Book } from './entities/book.entity';
@@ -10,15 +12,27 @@ export class BooksService {
   constructor(
     @InjectRepository(Book)
     private bookRepository: Repository<Book>,
+    @InjectRepository(Author)
+    private authorRepository: Repository<Author>,
+    @InjectRepository(Category)
+    private categoryRepository: Repository<Category>,
   ) {}
 
   async create(createBookDto: CreateBookDto) {
+    try {
+      await this.authorRepository.findOneByOrFail({ id: createBookDto.author });
+      await this.categoryRepository.findOneByOrFail({
+        id: createBookDto.category,
+      });
+    } catch (error) {
+      throw new HttpException(error.message, HttpStatus.BAD_REQUEST);
+    }
     try {
       const result = await this.bookRepository.save(createBookDto);
       return result;
     } catch (error) {
       if (error.code == '23505') {
-        throw new HttpException('Author already Exists!', HttpStatus.CONFLICT);
+        throw new HttpException('Book already Exists!', HttpStatus.CONFLICT);
       }
     }
   }
@@ -26,8 +40,9 @@ export class BooksService {
   // This is so ugly and unsafe to write raw SQL, TypeORM Docs are horrible.
 
   async findAll({ search, authorId, categoryId }: FilterBookDto) {
-    let queryString = `SELECT BOOK.NAME AS Name,
+    let queryString = `SELECT BOOK.ID AS ID, BOOK.NAME AS Name,AUTHOR.ID as AuthorID,
      AUTHOR.NAME AS Author,
+     CATEGORY.ID AS CategoryID,
       CATEGORY.NAME AS Category,
        BOOK.DESCRIPTION FROM BOOK, AUTHOR, CATEGORY
     WHERE
@@ -51,10 +66,7 @@ export class BooksService {
     }
     queryString += ';';
     console.log(queryString, dbQueryParams);
-    const books = await this.bookRepository.query(
-      queryString + ';',
-      dbQueryParams,
-    );
+    const books = await this.bookRepository.query(queryString, dbQueryParams);
 
     if (books.length === 0) {
       throw new HttpException('0 books found', HttpStatus.NOT_FOUND);
